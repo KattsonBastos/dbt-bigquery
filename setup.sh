b5c7feef2f769c0b9763f7d690e67d5f72c79206
Kattson Bastos <kattsonbastos@gmail.com>
2023-01-26 11:35:54 +0000

    build: saving default profile.yml

--- setup.sh
+++ setup.sh
@@ -2,7 +2,28 @@
 build() {
   echo "Installing dbt-bigquery..."
 
+  # install dbtbigquery
   pip install dbt-bigquery
+  
+  cp config_files/profiles.yml ~/.dbt/profiles.yml
+
+  dbt --version
+ 
+}
+
+up() {
+
+  echo "Enter the dbt project name you want"
+  read dbt_project_name
+
+  dbt init dbt_project_name
+ 
+}
+
+
+config() {
+
+  export GOOGLE_APPLICATION_CREDENTIALS='/tmp/my_key.json'
  
 }
 
@@ -10,7 +31,13 @@ case $1 in
   build)
     build
     ;;
+  up)
+    up
+    ;;
+  config)
+    config
+    ;;
   *)
-    echo "Usage: $0 {build}"
+    echo "Usage: $0 {build|up|config}"
     ;;
 esac
